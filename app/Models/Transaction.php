02b4442c8bd955dff839a44771692748b67d4052
iqbal <iqbaldhiaa@gmail.com>
2021-01-30 09:38:07 +0700

    Add models, migration, response formatters helpers

---
+++ app/Models/Transaction.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'food_id',
+        'user_id',
+        'quantity',
+        'total',
+        'status',
+        'payment_url'
+    ];
+
+    public function food()
+    {
+        return $this->hasOne(Food::class, 'id', 'food_id');
+    }
+
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timestamp;
+    }
+    public function getUpdatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timestamp;
+    }
+}
